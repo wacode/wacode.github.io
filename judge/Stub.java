1576f8176053bd56f88ba46be0e051f54f1e5466
Michael Colavita <colavitam@gmail.com>
2013-10-28 17:08:32 -0400

    Added stub to judge page.

---
+++ judge/Stub.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Stub {
+	public static void main(String[] args) {
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			String input = br.readLine();
+
+		}
+		catch(Exception e) {
+
+		}
+	}
+}
