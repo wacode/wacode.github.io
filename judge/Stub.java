0f54023c994fbf773483cf4f4206d01ea1f6c468
Michael Colavita <colavitam@gmail.com>
2013-10-28 17:09:21 -0400

    Revisions to stub.

--- judge/Stub.java
+++ judge/Stub.java
@@ -2,14 +2,9 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Stub {
-	public static void main(String[] args) {
-		try {
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			String input = br.readLine();
+	public static void main(String[] args) throws Throwable {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input = br.readLine();
 
-		}
-		catch(Exception e) {
-
-		}
 	}
 }
