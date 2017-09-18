61ad98d42e48a489dd81446be8992a437996a73c
Harshal Sheth <hsheth2@gmail.com>
2017-09-18 18:05:45 -0400

    Updated for 2017-18 year, and added Stub.java from judge folder

---
+++ Stub.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Stub {
+	public static void main(String[] args) throws Throwable {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input = br.readLine();
+
+	}
+}
